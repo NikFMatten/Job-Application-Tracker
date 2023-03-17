219bbb838e6b4edabc8786103ea26cdb612e3e62
NikFMatten <nfmatten@gmail.com>
2023-03-17 16:31:11 -0600

    Imported Routes and components to App file

--- jat-frontend/src/App.js
+++ jat-frontend/src/App.js
@@ -1,8 +1,15 @@
 import { Routes, Route } from "react-router-dom";
 
+// Components imports
+import NarBar from "./components/NavBar/NavBar";
+
+// Page imports
+import HomePage from "./pages/HomePage/HomePage";
+
 function App() {
   return (
     <div>
+      <NarBar />
       <Routes>
         <Route path="/" element={<HomePage />} />
       </Routes>
