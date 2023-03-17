32831aa7f1760ba6e490cee516d761e0548b82f0
NikFMatten <nfmatten@gmail.com>
2023-03-17 14:31:50 -0600

    created react app

---
+++ jat-frontend/src/App.js
@@ -0,0 +1,13 @@
+import { Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
