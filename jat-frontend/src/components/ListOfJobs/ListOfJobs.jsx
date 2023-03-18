0f20460bbf9573ac332b295e7f1965626c921bcb
NikFMatten <nfmatten@gmail.com>
2023-03-18 16:27:59 -0600

    feat: created a function to map all jobs to SingleJob component and displays all jobs

--- jat-frontend/src/components/ListOfJobs/ListOfJobs.jsx
+++ jat-frontend/src/components/ListOfJobs/ListOfJobs.jsx
@@ -1,7 +1,27 @@
-import React, { useState } from "react";
+import { Grid, Box, Paper } from "@mui/material";
+import React from "react";
+import SingleJob from "../SingleJob/SingleJob";
 
 const ListOfJobs = (props) => {
-  return <div></div>;
+  const { jobListings } = props;
+
+  return (
+    <div>
+      <Box sx={{ width: 300 }}>
+        <Paper elevation={2} sx={{ bgcolor: "#999999" }}>
+          <Grid container spacing={2} sx={{ pb: 5, px: 3 }}>
+            {[...jobListings].reverse().map((job) => {
+              return (
+                <Grid item sm={12} key={job.id}>
+                  <SingleJob job={job} />
+                </Grid>
+              );
+            })}
+          </Grid>
+        </Paper>
+      </Box>
+    </div>
+  );
 };
 
 export default ListOfJobs;
