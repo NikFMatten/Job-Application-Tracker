ab6fd161c2e8acca916472113df09b06efb9283e
NikFMatten <nfmatten@gmail.com>
2023-03-19 10:38:51 -0600

    feat: created a filter to filter saved jobs from today's date to 30 days ago

--- jat-frontend/src/components/ListOfJobs/ListOfJobs.jsx
+++ jat-frontend/src/components/ListOfJobs/ListOfJobs.jsx
@@ -6,13 +6,22 @@ import SingleJob from "../SingleJob/SingleJob";
 const ListOfJobs = (props) => {
   const { jobListings } = props;
 
+  const todaysDate = new Date();
+  const monthAgo = new Date();
+  monthAgo.setDate(monthAgo.getDate() - 30);
+
+  const results = jobListings.filter((d) => {
+    const time = new Date(d.date_applied);
+    return monthAgo < time && time < todaysDate;
+  });
+
   return (
     <div>
       <ColumnTitle text={"Applied Jobs"} color={"#fff"} />
       <Box sx={{ width: 300 }}>
         <Paper elevation={2} sx={{ bgcolor: "#999999" }}>
           <Grid container spacing={2} sx={{ pb: 5, px: 3 }}>
-            {[...jobListings].reverse().map((job) => {
+            {[...results].reverse().map((job) => {
               return (
                 <Grid item sm={12} key={job.id}>
                   <SingleJob job={job} />
