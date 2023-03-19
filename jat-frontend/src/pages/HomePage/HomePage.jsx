c21efe571a04f740bfba2bd891f5b13b6999ce6a
NikFMatten <nfmatten@gmail.com>
2023-03-19 13:14:32 -0600

    feat: added props from App.js and passed to ListOfJobs component + added ArchivedJobs component

--- jat-frontend/src/pages/HomePage/HomePage.jsx
+++ jat-frontend/src/pages/HomePage/HomePage.jsx
@@ -4,16 +4,23 @@ import Grid from "@mui/material/Grid";
 import ListOfJobs from "../../components/ListOfJobs/ListOfJobs";
 import NoJobData from "../../components/NoJobData/NoJobData";
 import MonthOldJobs from "../../components/MonthOldJobs/MonthOldJobs";
+import ArchivedJobs from "../../components/ArchivedJobs/ArchviedJobs";
 
 const HomePage = (props) => {
-  const { jobListings } = props;
+  const { jobListings, archivedJobs, deleteJobListing, addNewArchivedJob } =
+    props;
   return (
     <div>
       <Container maxWidth="lg" id="container">
         {jobListings.length > 0 ? (
           <Grid container spacing={2}>
-            <ListOfJobs jobListings={jobListings} />
+            <ListOfJobs
+              jobListings={jobListings}
+              deleteJobListing={deleteJobListing}
+              addNewArchivedJob={addNewArchivedJob}
+            />
             <MonthOldJobs jobListings={jobListings} />
+            <ArchivedJobs archivedJobs={archivedJobs} />
           </Grid>
         ) : (
           <NoJobData />
