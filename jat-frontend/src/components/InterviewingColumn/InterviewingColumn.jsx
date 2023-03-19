a453b0fd8dff8a10760fbec71e6a4596bd8569eb
NikFMatten <nfmatten@gmail.com>
2023-03-19 13:18:46 -0600

    feat: Created empty component to display jobs currently interviewing for

---
+++ jat-frontend/src/components/InterviewingColumn/InterviewingColumn.jsx
@@ -0,0 +1,10 @@
+import { Grid, Box, Paper } from "@mui/material";
+import React from "react";
+import ColumnTitle from "../ColumnTitle/ColumnTItle";
+import SingleJob from "../SingleJob/SingleJob";
+
+const InterviewingColumn = (props) => {
+    return (  );
+}
+ 
+export default InterviewingColumn;
