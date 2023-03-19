6ef9f067ee03bf30ccdc0d6d196933074fe65331
NikFMatten <nfmatten@gmail.com>
2023-03-19 13:25:01 -0600

    feat: created serializer class and file

---
+++ backend/interviewing/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from .models import Job
+
+class JobSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Job
+        fields = ['id', 'company_name', 'job_title', 'job_description', 'job_listing_url', 'salary_range', 'office_policy', 'job_location', 'date_applied', 'interview_type']
