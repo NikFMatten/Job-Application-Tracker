bf17ab8951181765aa0b9d5e72495e92f3f711fd
NikFMatten <nfmatten@gmail.com>
2023-03-19 13:53:41 -0600

    feat: created component to display jobs interviewing

--- jat-frontend/src/components/InterviewingColumn/InterviewingColumn.jsx
+++ jat-frontend/src/components/InterviewingColumn/InterviewingColumn.jsx
@@ -4,7 +4,30 @@ import ColumnTitle from "../ColumnTitle/ColumnTItle";
 import SingleJob from "../SingleJob/SingleJob";
 
 const InterviewingColumn = (props) => {
-    return (  );
-}
- 
+  const { interviewingJobs, deleteInterviewingJob, addNewArchivedJob } = props;
+  return (
+    <div>
+      <ColumnTitle text={"Interviewing"} color={"#7eec7e"} />
+      <Box sx={{ width: 300 }}>
+        <Paper elevation={2} sx={{ bgcolor: "#c2f6c2", minHeight: 1000 }}>
+          <Grid container spacing={2} sx={{ pb: 5, px: 3 }}>
+            {[...interviewingJobs].map((job) => {
+              return (
+                <Grid item sm={12} key={job.id}>
+                  <SingleJob
+                    job={job}
+                    deleteJobListing={deleteInterviewingJob}
+                    addNewArchivedJob={addNewArchivedJob}
+                    displayArchiveButton={true}
+                  />
+                </Grid>
+              );
+            })}
+          </Grid>
+        </Paper>
+      </Box>
+    </div>
+  );
+};
+
 export default InterviewingColumn;
