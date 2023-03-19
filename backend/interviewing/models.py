2459a301a7fe93701bdf12346b12a3e6d1cb4bf5
NikFMatten <nfmatten@gmail.com>
2023-03-19 13:20:58 -0600

    feat: created interviewing app in order to have jobs saved in separate table

---
+++ backend/interviewing/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.utils import timezone
+
+class Job(models.Model):
+    company_name = models.CharField(max_length=255)
+    job_title = models.CharField(max_length=255)
+    job_description = models.CharField(max_length=8000)
+    job_listing_url = models.URLField(max_length=255)
+    salary_range = models.CharField(max_length=25)
+    office_policy = models.CharField(max_length=10)
+    job_location = models.CharField(max_length=50)
+    date_applied = models.DateTimeField(default=timezone.now)
+    interview_type = models.CharField(max_length=255)
