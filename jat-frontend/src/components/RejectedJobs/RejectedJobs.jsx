51ca2af4654d5b05fc5319b124db38dd70ae6ffd
NikFMatten <nfmatten@gmail.com>
2023-03-19 18:00:09 -0600

    feat: created RejectedJobs component

---
+++ jat-frontend/src/components/RejectedJobs/RejectedJobs.jsx
@@ -0,0 +1,32 @@
+import { Grid, Box, Paper } from "@mui/material";
+import React from "react";
+import ColumnTitle from "../ColumnTitle/ColumnTItle";
+import SingleJob from "../SingleJob/SingleJob";
+
+const RejectedJobs = (props) => {
+  const { rejectedJobs, addRejectedJobs } = props;
+  return (
+    <div>
+      <ColumnTitle text={"Rejected"} color={"red"} />
+      <Box sx={{ width: 300 }}>
+        <Paper elevation={2} sx={{ bgcolor: "#ffb1b1", minHeight: 1000 }}>
+          <Grid container spacing={2} sx={{ pb: 5, px: 3 }}>
+            {[...rejectedJobs].reverse().map((job) => {
+              return (
+                <Grid item sm={12} key={job.id}>
+                  <SingleJob
+                    job={job}
+                    displayArchivedJob={false}
+                    interviewing={false}
+                  />
+                </Grid>
+              );
+            })}
+          </Grid>
+        </Paper>
+      </Box>
+    </div>
+  );
+};
+
+export default RejectedJobs;
